d443ecdd9db65e6a6b888735be961f744653d4b8
pivotal-khurrum-mujeeb <kabdul@pivotal.io>
2017-06-16 17:48:52 -0400

    19-151

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -35,7 +35,6 @@ class EmployeeList extends Component {
   render() {
     return (
       <ListView
-        enableEmptySections
         dataSource={this.dataSource}
         renderRow={this.renderRow}
       />
